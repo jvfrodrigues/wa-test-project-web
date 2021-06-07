ecc790ca85a1d2a0a2754fe721b87cc9da85ee50
Joao Rodrigues <jvfr96@gmail.com>
2021-06-06 22:18:07 -0300

    chore: define Request form dialog

---
+++ src/components/Pages/Admin/Requests/FormDialog.tsx
@@ -0,0 +1,111 @@
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Grid from '@material-ui/core/Grid';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Slide from '@material-ui/core/Slide';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import TextField from 'components/Shared/Fields/Text';
+import Toast from 'components/Shared/Toast';
+import { logError } from 'helpers/rxjs-operators/logError';
+import { useFormikObservable } from 'hooks/useFormikObservable';
+import IRequest from 'interfaces/models/request';
+import React, { forwardRef, Fragment, memo, useCallback } from 'react';
+import { tap } from 'rxjs/operators';
+import requestService from 'services/request';
+import * as yup from 'yup';
+
+interface IProps {
+  opened: boolean;
+  request?: IRequest;
+  onComplete: (request: IRequest) => void;
+  onCancel: () => void;
+}
+
+const validationSchema = yup.object().shape({
+  quantity: yup.number().required(),
+  description: yup.string().required().min(10).max(200)
+});
+
+const useStyle = makeStyles({
+  content: {
+    width: 600,
+    maxWidth: 'calc(95vw - 50px)'
+  },
+  heading: {
+    marginTop: 20,
+    marginBottom: 10
+  }
+});
+
+const FormDialog = memo((props: IProps) => {
+  const classes = useStyle(props);
+
+  const formik = useFormikObservable<IRequest>({
+    validationSchema,
+    onSubmit(model) {
+      return requestService.save({ ...model, quantity: Number(model.quantity) }).pipe(
+        tap(request => {
+          Toast.show('Novo pedido criado com sucesso');
+          props.onComplete(request);
+        }),
+        logError(true)
+      );
+    }
+  });
+
+  const handleEnter = useCallback(() => {
+    formik.setValues(props.request ?? formik.initialValues, false);
+  }, [formik, props.request]);
+
+  const handleExit = useCallback(() => {
+    formik.resetForm();
+  }, [formik]);
+
+  return (
+    <Dialog
+      open={props.opened}
+      disableBackdropClick
+      disableEscapeKeyDown
+      onEnter={handleEnter}
+      onExited={handleExit}
+      TransitionComponent={Transition}
+    >
+      {formik.isSubmitting && <LinearProgress color='primary' />}
+
+      <form onSubmit={formik.handleSubmit}>
+        <DialogTitle>{formik.values.id ? 'Editar' : 'Novo'} Pedido</DialogTitle>
+        <DialogContent className={classes.content}>
+          <Fragment>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={12}>
+                <TextField label='Quantidade' name='quantity' formik={formik} />
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={12}>
+                <TextField label='Descrição' name='description' formik={formik} />
+              </Grid>
+            </Grid>
+          </Fragment>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={props.onCancel}>Cancelar</Button>
+          <Button color='primary' variant='contained' type='submit' disabled={formik.isSubmitting}>
+            Salvar
+          </Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+});
+
+const Transition = memo(
+  forwardRef((props: any, ref: any) => {
+    return <Slide direction='up' {...props} ref={ref} />;
+  })
+);
+
+export default FormDialog;
